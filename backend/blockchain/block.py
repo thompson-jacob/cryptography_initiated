22591b0b494618d96693d17717b2821636785eb0
thompson-jacob <63691359+thompson-jacob@users.noreply.github.com>
2021-04-13 15:35:13 -0400

    adding mining methods

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -6,39 +6,52 @@ GENESIS_DATA = {
   'timestamp': 1,
   'last_hash': 'genesis_last_hash',
   'hash': 'genesis_hash',
-  'data': []
+  'data': [], 
+  'difficulty': 3,
+  'nonce': 'genesis_nonce',
 }
 class Block: 
   """
   Block: A unit of storage.
   Stores transactions in blockchain that supports a cryptocurrency.
   """
-  def __init__(self, timestamp, last_hash, hash, data):
+  def __init__(self, timestamp, last_hash, hash, data, difficulty, nonce):
     self.timestamp = timestamp
     self.last_hash = last_hash
     self.hash = hash
     self.data = data
+    self.difficulty = difficulty
+    self.nonce = nonce
 
   def __repr__(self): 
-    # python
+    # python special method for string representation
     return (
       'Block('
       f'timestamp: {self.timestamp}, '
       f'last_hash: {self.last_hash}, '
       f'hash: {self.hash}, '
-     f'Block - data: {self.data}'
+      f'Block - data: {self.data},'
+      f'difficulty: {self.difficulty},'
+      f'nonce: {self.nonce})'
+
     )
 
   @staticmethod
   def mine_block(last_block, data):
     """
-    Mine a block based on the given last_block and data.
+    Mine a block based on the given last_block and data, until a block hash
+    is found that meets the leading 0's proof of work requirement.
     """
     timestamp = time.time_ns()
     last_hash = last_block.hash
-    hash = crypto_hash(timestamp, last_hash, data)
+    difficulty = last_block.difficulty
+    nonce = 0
+    hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
-    return Block(timestamp, last_hash, hash, data)
+    while hash[0:difficulty] != '0' * difficulty:
+
+
+    return Block(timestamp, last_hash, hash, data, difficulty, nonce)
 
   @staticmethod
   def genesis():
@@ -50,6 +63,7 @@ class Block:
     #   GENESIS_DATA['gensis_last_hash'],
     #   GENESIS_DATA['hash'],
     #   GENESIS_DATA['data']
+    #   added Genesis data for difficulty and nonce
     # )
 
     return Block(**GENESIS_DATA)
